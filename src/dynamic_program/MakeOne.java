2de2c34ab8ca8febc73d85dcb074c6cf4b9198a8
dltmdan92 <dltmdan92@naver.com>
2021-10-02 20:45:30 +0900

    1로 만들기 문제 - 다시풀기

--- src/dynamic_program/MakeOne.java
+++ src/dynamic_program/MakeOne.java
@@ -1,9 +1,8 @@
 package dynamic_program;
 
-import java.util.PriorityQueue;
-
 /**
  * 1로 만들기 문제
+ *
  *
  * 이거는 그리디 알고리즘에서의 1만들기와는 다르다. 단순 그리디 해법으로 해결하기 힘들다.
  * 그리디에서는 무조건 나누기를 먼저, 최대한 할려고 했음
@@ -13,30 +12,58 @@ import java.util.PriorityQueue;
  * 다이나믹 프로그래밍은 점화식으로 풀어라라 */
 public class MakeOne {
     public static void main(String[] args) {
-        int[] d = new int[30001];
-        int x = 33;
-        d[2] = 1;
-        d[3] = 1;
-        d[5] = 1;
+        int param = 27;
+        int[] memory = new int[param + 1];
 
-        for (int i = 2; i <= x; i++) {
-            d[i] = d[i - 1] + 1;
-            // 현재의 수가 2로 나누어 떨어지는 경우
-            if (i % 2 == 0) {
-                d[i] = Math.min(d[i], d[i/2] + 1);
-            }
-            // 현재의 수가 3로 나누어 떨어지는 경우
-            if (i % 3 == 0) {
-                d[i] = Math.min(d[i], d[i/3] + 1);
-            }
-            // 현재의 수가 5로 나누어 떨어지는 경우
-            if (i % 5 == 0) {
-                d[i] = Math.min(d[i], d[i/5] + 1);
-            }
-            System.out.println("d["+i+"] : " + d[i]);
+        memory[1] = 0;
+        memory[2] = 1;
+        memory[3] = 1;
+        memory[4] = 2;
+        memory[5] = 1;
+
+        if (param <= 5) {
+            System.out.println(memory[param]);
+            return;
         }
 
-        System.out.println(d[x]);
+        int index = 6;
+        while (index <= param) {
+            int divFive = Integer.MAX_VALUE;
+            int divThree = Integer.MAX_VALUE;
+            int divTwo = Integer.MAX_VALUE;
+            int minusOne = memory[index - 1];
+
+            if (index % 5 == 0) {
+                divFive = memory[index / 5];
+            }
+            if (index % 3 == 0) {
+                divThree = memory[index / 3];
+            }
+            if (index % 2 == 0) {
+                divTwo = memory[index / 2];
+            }
+
+            int min = minOfFour(divFive, divThree, divTwo, minusOne);
+
+            memory[index] = min + 1;
+
+            index++;
+        }
+
+        System.out.println(memory[param]);
+
+    }
+
+    private static int minOfFour(int divFive, int divThree, int divTwo, int minusOne) {
+        int min = divFive;
+
+        if (min > divThree) min = divThree;
+
+        if (min > divTwo) min = divTwo;
+
+        if (min > minusOne) min = minusOne;
+
+        return min;
     }
 
 
