7c7d2991dc1239e853107ad8299f4eaac9a9d1e3
dltmdan92 <dltmdan92@naver.com>
2022-01-08 12:42:23 +0900

    서로소 집합 알고리즘 다시 풀기

--- src/graph/SeorosoSet.java
+++ src/graph/SeorosoSet.java
@@ -12,34 +12,44 @@ public class SeorosoSet {
         // 간선의 개수
         int e = 4;
         // 부모 테이블 초기화
-        int[] parent = new int[v + 1];
+        int[] parents = new int[v + 1];
 
-        // 부모 테이블 상에서, 부모를 자기 자신으로 초기화
-        for (int i = 1; i < parent.length; i++) {
-            parent[i] = i;
+        // STEP 1
+        // 각각의 노드의 부모를 자기 자신으로 설정
+        for (int i = 1; i < parents.length ; i++) {
+            parents[i] = i;
         }
 
-        // union 연산을 각각 수행
-        // 이 부분은 생략
+        union(1, 4, parents);
+        union(2, 3, parents);
+        union(2, 4, parents);
+        union(5, 6, parents);
+
+        for (int i = 1, parentsLength = parents.length; i < parentsLength; i++) {
+            System.out.println(i + " : " + findRoot(i, parents));
+        }
+
     }
 
-    // 특정 원소가 속한 집합의 루트 노드를 찾기
-    private int findParent(int[] parent, int x) {
-        // 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
-        if (parent[x] != x)
-            //return findParent(parent, parent[x]); // find 할때마다 O(V)
-            parent[x] = findParent(parent, parent[x]); // parent를 갱신 해줌으로써 O(1)
-        return parent[x];
+    public static void union(int i, int j, int[] parents) {
+        int rootOfI = findRoot(i, parents);
+        int rootOfJ = findRoot(j, parents);
+
+        if (rootOfI > rootOfJ) {
+            parents[rootOfI] = rootOfJ;
+        }
+        else {
+            parents[rootOfJ] = rootOfI;
+        }
     }
 
-    // 두 원소가 속한 집합을 합치기
-    private void unionParent(int[] parent, int a, int b) {
-        int node_a = findParent(parent, a);
-        int node_b = findParent(parent, b);
-        if (node_a < node_b)
-            parent[b] = node_a;
-        else
-            parent[a] = node_b;
+    private static int findRoot(int i, int[] parents) {
+        int parent = parents[i];
+        if (parent == i) {
+            return parent;
+        }
+        return findRoot(parent, parents);
     }
+
 
 }
