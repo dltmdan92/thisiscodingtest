e9481784f31df969ff41b6817619da9ced7286aa
dltmdan92 <dltmdan92@naver.com>
2023-06-17 19:42:57 +0900

    [DFS] 음료수 얼려먹기

--- src/dfs/IceBeverage.java
+++ src/dfs/IceBeverage.java
@@ -5,54 +5,70 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class IceBeverage {
-    private static int rows, cols;
+    // 상 하 좌 우
+    private static final int[] drow = new int[] {-1,1,0,0};
+    private static final int[] dcol = new int[] {0,0,-1,1};
+    private static int rowLength;
+    private static int colLength;
+
 
     public static void main(String[] args) {
-        String firstParam = IceBeverageParamKt.getFirstParam();
-        String secondParam = IceBeverageParamKt.getSecondParam();
+        String param = """
+                001
+                010
+                101
+                """;
 
-        String[] rowsAndCols = firstParam.split(" ");
-        rows = Integer.parseInt(rowsAndCols[0]);
-        cols = Integer.parseInt(rowsAndCols[1]);
+        List<List<Integer>> map = param.lines()
+                .map(line -> Arrays.stream(line.split(""))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList()))
+                .toList();
 
-        String[] lines = secondParam.split("\n");
-        List<List<Integer>> data = Arrays.stream(lines)
-                .map(line -> {
-                    String[] split = line.split("");
-                    return Arrays.stream(split).map(Integer::parseInt).collect(Collectors.toList());
-                })
-                .collect(Collectors.toList());
+        rowLength = map.size();
+        colLength = map.get(0).size();
 
-        int result = 0;
+        int count = 0;
 
-        for (int n = 0; n < rows; n++) {
-            for (int m = 0; m < cols; m++) {
-                if (dfs(data, n, m)) result++;
+        for (int i = 0; i < rowLength; i++) {
+            for (int j = 0; j < colLength; j++) {
+                if (map.get(i).get(j) == 0) {
+                    DFS(map, i, j);
+                    count++;
+                }
             }
         }
 
-        System.out.println(result);
+        System.out.println(count);
     }
 
-    private static boolean dfs(List<List<Integer>> data, int row, int col) {
+    private static void DFS(List<List<Integer>> map, int row, int col) {
+        for (int i = 0; i < drow.length; i++) {
+            int newRow = row + drow[i];
+            int newCol = col + dcol[i];
 
-        if ((row < 0 || col < 0) ||
-                (row >= rows || col >= cols)
-        ) {
-            return false;
+            if (newRow >= 0 && newRow < rowLength && newCol >= 0 && newCol < colLength && map.get(newRow).get(newCol) == 0) {
+                map.get(newRow).set(newCol, 1);
+                DFS(map, newRow, newCol);
+            }
+        }
+    }
+
+    public static class Node {
+        private final int row;
+        private final int col;
+
+        public Node(int row, int col) {
+            this.row = row;
+            this.col = col;
         }
 
-        if (data.get(row).get(col) == 1) {
-            return false;
+        public int getRow() {
+            return row;
         }
 
-        data.get(row).set(col, 1);
-
-        dfs(data, row + 1, col);
-        dfs(data, row, col + 1);
-        dfs(data, row - 1, col);
-        dfs(data, row, col - 1);
-
-        return true;
+        public int getCol() {
+            return col;
+        }
     }
 }
