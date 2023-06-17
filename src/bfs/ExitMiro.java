d752fbdb3d1d76eb719cceb1e80862128572bc89
dltmdan92 <dltmdan92@naver.com>
2023-06-17 20:16:28 +0900

    [BFS] 미로 탈출

--- src/bfs/ExitMiro.java
+++ src/bfs/ExitMiro.java
@@ -1,80 +1,76 @@
 package bfs;
 
+import implementation.UpDownLeftRight;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class ExitMiro {
     private static int rows, cols;
 
+    private static final int[] dRow = {1, -1, 0, 0};
+    private static final int[] dCol = {0, 0, 1, -1};
+    private static int destinationRow;
+    private static int destinationCol;
+
     public static void main(String[] args) {
-        String[] rowsAndCols = ExitMiroParamKt.firstParam().split(" ");
-        rows = Integer.parseInt(rowsAndCols[0]);
-        cols = Integer.parseInt(rowsAndCols[1]);
+        String firstParam = "5 6";
+        String secondParam = """
+                101010
+                111111
+                000001
+                111111
+                111111
+                """;
 
-        String[] lines = ExitMiroParamKt.secondParam().split("\n");
+        int[] destination = Arrays.stream(firstParam.split(" ")).mapToInt(Integer::parseInt).toArray();
+        destinationRow = destination[0] - 1;
+        destinationCol = destination[1] - 1;
 
-        List<List<Integer>> data = Arrays.stream(lines)
-                .map(line -> line.split(""))
-                .map(line -> Arrays.stream(line).map(Integer::parseInt).collect(Collectors.toList()))
-                .collect(Collectors.toList());
+        System.out.println("destinationRow : " + destinationRow);
+        System.out.println("destinationCol : " + destinationCol);
 
-        int[] dRow = {1, -1, 0, 0};
-        int[] dCol = {0, 0, 1, -1};
+        List<List<Integer>> map = secondParam.lines()
+                .map(line -> Arrays.stream(line.split("")).map(Integer::parseInt).collect(Collectors.toList()))
+                .toList();
 
-        Element firstElement = new Element(0, 0);
-        Element resultElement = null;
+        Element start = new Element(0, 0);
 
         Queue<Element> queue = new LinkedList<>();
-
-        queue.add(firstElement);
+        queue.add(start);
 
         while (!queue.isEmpty()) {
-            Element element = queue.poll();
+            System.out.println("queue");
+            Element visitedElement = queue.poll();
 
-            int nowRow = element.getRow();
-            int nowCol = element.getCol();
-            int nowValue = data.get(nowRow).get(nowCol);
-
-            // 목적지 도달 시 break
-            if (isDestination(nowRow, nowCol)) {
-                resultElement = element;
-                break;
-            }
-
-            for (int i = 0; i < 4; i++) {
-                int newRow = nowRow + dRow[i];
-                int newCol = nowCol + dCol[i];
-
-                if(!isValidNewPosition(newRow, newCol)) continue;
-
-                Integer newPositionVal = data.get(newRow).get(newCol);
-
-                if (newPositionVal != 1) continue;
-
-                // 새로 가는 곳의 값을 1씩 증가
-                data.get(newRow).set(newCol, nowValue+1);
-
-                queue.add(new Element(newRow, newCol));
+            for (int i = 0; i < dRow.length; i++) {
+                int newRow = visitedElement.getRow() + dRow[i];
+                int newCol = visitedElement.getCol() + dCol[i];
+                System.out.println("newRow: " + newRow + ", newCol: " + newCol);
+                System.out.println("map.size() : " + map.size());
+                System.out.println("map.get(0).size() : " + map.get(0).size());
+                System.out.println("(newRow != 0 && newCol != 0) : " + (newRow != 0 && newCol != 0));
+                if (newRow >= 0 && newRow < map.size() && newCol >= 0 && newCol < map.get(0).size()
+                        && (newRow != 0 || newCol != 0)
+                        && (map.get(newRow).get(newCol) == 1)
+                ) {
+                    Integer nowVal = map.get(visitedElement.getRow()).get(visitedElement.getCol());
+                    queue.add(new Element(newRow, newCol));
+                    map.get(newRow).set(newCol, nowVal + 1);
+                    System.out.println(map.get(newRow).get(newCol));
+                }
+                if (newRow == destinationRow && newCol == destinationCol) {
+                    System.out.println("break");
+                    break;
+                }
             }
         }
 
-        assert resultElement != null;
-        int result = data.get(resultElement.getRow()).get(resultElement.getCol());
-
-        System.out.println(result);
-    }
-
-    private static boolean isDestination(int nowRow, int nowCol) {
-        return (nowRow == rows - 1) && (nowCol == cols - 1);
-    }
-
-    private static boolean isValidNewPosition(int newRow, int newCol) {
-        return (newRow >= 0 && newRow < rows) && (newCol >= 0 && newCol < cols);
+        System.out.println(map.get(destinationRow).get(destinationCol));
     }
 
 
     private static class Element {
-
         private final int row;
         private final int col;
 
@@ -89,19 +85,6 @@ public class ExitMiro {
         public Element(int row, int col) {
             this.row = row;
             this.col = col;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Element element = (Element) o;
-            return row == element.row && col == element.col;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(row, col);
         }
     }
 }
