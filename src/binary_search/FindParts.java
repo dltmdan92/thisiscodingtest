df6208e9485e52b5addb4e272b05cd2bfee87c23
dltmdan92 <dltmdan92@naver.com>
2023-06-18 20:52:16 +0900

    [binary search] 부품 찾기 문제

--- src/binary_search/FindParts.java
+++ src/binary_search/FindParts.java
@@ -1,53 +1,34 @@
 package binary_search;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Arrays;
 
 public class FindParts {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = Files.newBufferedReader(Path.of("/Users/seungmoo.lee/IdeaProjects/thisiscodingtest/src/binary_search/FindParts.txt"));
-
-        int n = Integer.parseInt(bufferedReader.readLine());
-        int[] parts = Arrays.stream(bufferedReader.readLine().split(" "))
-                .mapToInt(Integer::valueOf)
-                .toArray();
-
-        int m = Integer.parseInt(bufferedReader.readLine());
-        int[] requiredParts = Arrays.stream(bufferedReader.readLine().split(" "))
-                .mapToInt(Integer::valueOf)
-                .toArray();
+        int n = 5;
+        int[] parts = {8, 3, 7, 9, 2};
+        int m = 3;
+        int[] required = {5, 7, 9, 8};
 
         Arrays.sort(parts);
 
-        for (int requiredPart : requiredParts) {
-            int start = 0;
-            int end = parts.length - 1;
-            boolean partFinded = binarySearch(parts, start, end, requiredPart);
-            System.out.println((partFinded) ? "yes" : "no");
+        for (int require : required) {
+            boolean binarySearch = binarySearch(parts, require, 0, n - 1);
+            if (binarySearch) System.out.println("yes");
         }
     }
 
-    private static boolean binarySearch(int[] parts, int start, int end, int requiredPart) {
-        if (start > end) {
-            return false;
-        }
+    private static boolean binarySearch(int[] parts, int require, int startIndex, int endIndex) {
+        int midIndex = (startIndex + endIndex) / 2;
+        System.out.printf("midIndex: %d, parts[midIndex] : %d\n", midIndex, parts[midIndex]);
+        if (parts[midIndex] == require) return true;
+        if (startIndex == endIndex) return false;
 
-        int mid = (start + end) / 2;
-
-        if (parts[mid] == requiredPart) {
-            return true;
-        }
-
-        if (parts[mid] > requiredPart) {
-            return binarySearch(parts, start, mid - 1, requiredPart);
-        }
-        else {
-            return binarySearch(parts, mid + 1, end, requiredPart);
+        if (parts[midIndex] < require) {
+            return binarySearch(parts, require, midIndex+1, endIndex);
+        } else {
+            return binarySearch(parts, require, startIndex, midIndex-1);
         }
     }
-
 }
